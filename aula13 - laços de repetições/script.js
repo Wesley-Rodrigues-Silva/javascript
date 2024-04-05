25052e1d778ca70ce94b427aec2562e3cd9720a5
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-04-04 22:02:15 -0300

    aula 14 - lacos de repeticao

---
+++ aula13 - laços de repetições/script.js
@@ -0,0 +1,21 @@
+var cont = 1 /*(quantas vezes vai acontecer)*/
+while (cont <= 6) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+
+
+/*var cont = 1 Laço de repetição de cima pra baixo (ao contrário) 
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 3) */
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */

4f8ec88fe3d943531f66f0c4759460cfb0822c92
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-04-06 21:00:48 -0300

    att

--- exercicios/desafio04/script.js
+++ exercicios/desafio04/script.js
@@ -1,23 +1,21 @@
 function contar() {
-    var ini = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
+    var ini = parseInt(document.getElementById('txti').value)
+    var fim = parseInt(document.getElementById('txtf').value)
+    var passo = parseInt(document.getElementById('txtp').value)
     var res = document.getElementById('res')
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    if (ini.length == 0 || fim.length == 0 || passo.length == 0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
         res.innerHTML = 'Contando: '
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-        if (i < f) {
-            for(var c = i; c <= f; c+= p) {
+        // Contagem crescente
+        if (ini < fim) {           
+            for(var c = ini; c <= fim; c+= passo) {
                 res.innerHTML += ` ${c} \u{1F449}` 
         }
-        
+        // Contagem decrescente
         } else {
-            for (c = i; c >= f; c-=p) {
+            for (c = ini; c >= fim; c-= passo) {
                 res.innerHTML += ` ${c} \u{1F449}`
             }           
         }

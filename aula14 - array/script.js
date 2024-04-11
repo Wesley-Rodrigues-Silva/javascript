1db6fae8f8710a7575b0a61d104df62faceb3318
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-04-10 21:12:10 -0300

    array

---
+++ aula14 - array/script.js
@@ -0,0 +1,26 @@
+let num = [5,8,2,9,3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valro do vetor é ${num[0]}`)
+let pos = num.indexOf(4)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
+
+    
+/*
+num.push(1)
+num.sort()
+for (let pos=0;pos<num.length;pos++){
+    console.log(`O valor ${pos} está na posição ${num[pos]}`)
+}
+
+for(let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+*/
+

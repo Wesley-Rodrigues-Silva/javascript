21624aaba86719dd1b8465ba5cb127c68de1726f
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-04-16 20:54:40 -0300

    att

---
+++ exercicios/desafio05/script.js
@@ -0,0 +1,45 @@
+var num = document.getElementById('fnum')
+var lista = document.getElementById('flista')
+var res = document.getElementById('res')
+var valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        
+    } else {
+        alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert('Adiciona valores antes de finalizar!')
+    } else {
+        var total = valores.length
+
+        res.innerHTML += `Ao todo temos ${total} números cadastrados </p>`
+    }
+}
